ad75243074407d76333de1f61767c2e7b8491112
Micheal Leverton <levertonmicheal@gmail.com>
2022-12-31 20:13:59 -0500

    asdfs

--- src/components/FFlottery.js
+++ src/components/FFlottery.js
@@ -19,7 +19,7 @@ function FFlottery() {
       <div className="container accordcontain">
         {/* <Contractinfo /> */}
         <div className="accordcontainer">
-        <div className="refresher"><a href="/#" onClick={() => {
+        <div className="refresher"><a href="#/" onClick={() => {
 								panelrefresh();
 							}}>Refresh</a></div>
           <Readaccordion />
